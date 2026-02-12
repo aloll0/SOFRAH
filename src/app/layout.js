9db83a2f4a27806afeb1dd44398c56d035e872a5
aloll <aw3043622@gmail.com>
2026-02-13 01:59:00 +0200

    edit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,28 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
+import Header from "../app/components/Header/Header";
+import Hero from "../app/components/Hero/Hero";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const outfit = Outfit({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-outfit",
 });
 
 export const metadata = {
   title: "SOFRAH",
   description: "food restaurant",
-  icons: [{ rel: "icon", url: "/logo (1).png" }],
+  icons: [{ rel: "icon", url: "/logo.png" }],
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${outfit.variable} antialiased`}
       >
+        <Header />
+        <Hero />
         {children}
       </body>
     </html>
